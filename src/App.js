e070f4d939ceda88ac652acc12caa84d54c3ebf4
Ampdrew06 <ampdrew@gmail.com>
2026-05-08 08:28:28 +0100

    Auto Global Pricing

--- src/App.js
+++ src/App.js
@@ -1,5 +1,21 @@
+import { useEffect } from "react";
 import AppRoutes from "./app/routes";
+import { loadMaterialsFromCloud } from "./lib/materials";
 
 export default function App() {
+  useEffect(() => {
+    async function syncMaterials() {
+      const ok = await loadMaterialsFromCloud();
+
+      console.log(
+        ok
+          ? "Global materials sync complete"
+          : "Using cached local materials"
+      );
+    }
+
+    syncMaterials();
+  }, []);
+
   return <AppRoutes />;
 }
